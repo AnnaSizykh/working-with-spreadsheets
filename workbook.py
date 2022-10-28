9a94404c75fcc446e36e0dd0e327e523371bf628
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2022-10-28 14:22:20 +0300

    Sheet choosing function logic changed
    Sheet choosing function allows to change a list while working

--- workbook.py
+++ workbook.py
@@ -21,11 +21,11 @@ class Workbook:
         """
         Opens an excel file from a working directory
         """
-        self.wb_name = input()
+        self.wb_name = input('Input file name')
         if not self.get_file_name():
             self.wb_name = 'demo'
         if Path(fr'.\work\{self.get_file_name()}.xlsx').exists() is False:
-            raise FileNotFoundError ("The file you're looking for does not exist")
+            raise FileNotFoundError("The file you're looking for does not exist")
         self.work_book = openpyxl.load_workbook(Path('.') / 'work' / f'{self.get_file_name()}.xlsx')
         self.sheets = self.work_book.sheetnames
 
@@ -33,24 +33,29 @@ class Workbook:
         """
         Allows user to choose a sheet from a current workbook
         """
-        while self.work_sheet is None:
+        while True:
             choose_ws = input(f'The file "{self.get_file_name()}" '
                               f'contains the next spreadsheets: {self.get_sheets_names()}\n'
                               'Please, choose one of the available sheets '
                               'by entering its name or ordinal number: ')
             if choose_ws == 'stop':
-                raise KeyboardInterrupt
+                print('Program stopped.')
+                return None
             try:
                 self.work_sheet = self.get_wb()[self.get_sheets_names()[int(choose_ws) - 1]]
+                break
             except IndexError:
-                print('List index out of range.\n'\
+                print('List index out of range.\n '
                       'Please, try again or enter "stop" to stop searching.')
             except ValueError:
                 if choose_ws not in self.get_sheets_names():
-                    print('The sheet you are looking for was not found.\n'\
+                    print('The sheet you are looking for was not found.\n'
                           'Please, try again or enter "stop" to stop searching.')
                 else:
                     self.work_sheet = self.get_wb()[choose_ws]
+                    break
+        print(f'Current worksheet is {self.get_worklist()}')
+        return 0
 
     def save(self):
         """
